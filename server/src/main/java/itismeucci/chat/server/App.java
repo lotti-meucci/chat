3d6d0b781d3ec6e131861a5fdec17901ac1f4885
LorenzoLotti <LorenzoLotti@users.noreply.github.com>
2023-01-06 18:43:16 +0100

    final source commit

--- server/src/main/java/itismeucci/chat/server/App.java
+++ server/src/main/java/itismeucci/chat/server/App.java
@@ -1,7 +1,12 @@
 package itismeucci.chat.server;
 
-public class App
+public final class App
 {
+	private App()
+	{
+		throw new UnsupportedOperationException();
+	}
+
 	public static void main(String[] args) throws Exception
 	{
 		System.out.println("Server in ascolto sulla porta " + ChatServer.PORT);
